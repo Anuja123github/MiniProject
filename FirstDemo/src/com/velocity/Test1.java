67ba14462ebc979405df89ef557b0bd6f0d73891
Anuja <anujakamble@vctpune.co.in>
2024-05-30 16:59:15 +0530

    added array list code

---
+++ FirstDemo/src/com/velocity/Test1.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ArrayList al= new ArrayList();
+al.add(10);
+al.add(20);
+System.out.println(al);
+	}
+
+}
