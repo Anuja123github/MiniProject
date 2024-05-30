0b32dacbd6ce04d1843c129d6af7307fd0ffe937
Anuja <anujakamble@vctpune.co.in>
2024-05-30 17:05:15 +0530

    added some change in genrices

--- FirstDemo/src/com/velocity/Test1.java
+++ FirstDemo/src/com/velocity/Test1.java
@@ -6,9 +6,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-ArrayList al= new ArrayList();
-al.add(10);
-al.add(20);
+ArrayList<String> al= new ArrayList<String>();
+al.add("10");
+al.add("20");
 System.out.println(al);
 	}
 
